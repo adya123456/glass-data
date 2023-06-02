85c949dbf129277971ef64c1df2540859c6bf105
adya123456 <90205748+adya123456@users.noreply.github.com>
2023-06-02 10:14:45 -0500

    Update Glass_APP.py

--- Glass_APP.py
+++ Glass_APP.py
@@ -172,3 +172,44 @@ k = st.sidebar.slider("Input K", float(glass_df['K'].min()), float(glass_df['K']
 ca = st.sidebar.slider("Input Ca", float(glass_df['Ca'].min()), float(glass_df['Ca'].max()))
 ba = st.sidebar.slider("Input Ba", float(glass_df['Ba'].min()), float(glass_df['Ba'].max()))
 fe = st.sidebar.slider("Input Fe", float(glass_df['Fe'].min()), float(glass_df['Fe'].max()))
+
+st.sidebar.subheader("Choose Classifier")
+classifier = st.sidebar.selectbox("Classifier", 
+                                 ('Support Vector Machine', 'Random Forest Classifier', 'Logistic Regression'))
+if classifier == 'Support Vector Machine':
+    st.sidebar.subheader("Model Hyperparameters")
+    c_value = st.sidebar.number_input("C (Error Rate)", 1, 100, step = 1)
+    kernel_input = st.sidebar.radio("Kernel", ("linear", "rbf", "poly"))
+    gamma_input = st. sidebar.number_input("Gamma", 1, 100, step = 1)
+
+    # If the user clicks 'Classify' button, perform prediction and display accuracy score and confusion matrix.
+    # This 'if' statement must be inside the above 'if' statement.
+if st.sidebar.button('Classify'):
+     st.subheader("Support Vector Machine")
+     svc_model = SVC(C = c_value, kernel = kernel_input, gamma = gamma_input)
+     svc_model.fit(X_train,y_train)
+     y_pred = svc_model.predict(X_test)
+     accuracy = svc_model.score(X_test, y_test)
+     glass_type = prediction(svc_model, ri, na, mg, al, si, k, ca, ba, fe)
+     st.write("The Type of glass predicted is:", glass_type)
+     st.write("Accuracy", accuracy.round(2))
+     plot_confusion_matrix(svc_model, X_test, y_test)
+     st.pyplot()
+     
+if classifier == 'Random Forest Classifier':
+    st.sidebar.subheader("Model Hyperparameters")
+    n_estimators_input = st.sidebar.number_input("Number of trees in the forest", 100, 5000, step = 10)
+    max_depth_input = st.sidebar.number_input("Maximum depth of the tree", 1, 100, step = 1)
+
+    # If the user clicks 'Classify' button, perform prediction and display accuracy score and confusion matrix.
+    # This 'if' statement must be inside the above 'if' statement. 
+if st.sidebar.button('Classify'):
+        st.subheader("Random Forest Classifier")
+        rf_clf = RandomForestClassifier(n_estimators = n_estimators_input, max_depth = max_depth_input, n_jobs = -1)
+        rf_clf.fit(X_train,y_train)
+        accuracy = rf_clf.score(X_test, y_test)
+        glass_type = prediction(rf_clf, ri, na, mg, al, si, k, ca, ba, fe)
+        st.write("The Type of glass predicted is:", glass_type)
+        st.write("Accuracy", accuracy.round(2))
+        plot_confusion_matrix(rf_clf, X_test, y_test)
+        st.pyplot()
